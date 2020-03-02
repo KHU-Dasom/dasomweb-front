412979a42fc5b7b32e6afef43c40e100c92c5ff6
inhun <torihome1@naver.com>
2020-03-02 19:54:38 +0900

    fix

--- src/components/Tables/ProfileTable.vue
+++ src/components/Tables/ProfileTable.vue
@@ -2,8 +2,14 @@
   <div>
     <md-table v-model="profile" :table-header-color="tableHeaderColor">
       <md-table-row slot="md-table-row" slot-scope="{ item }">
-        {{ item.name }}
-        {{ item.enrollYear }}
+        <div v-if="login">
+          <li><a>{{ item.name }}</a></li>
+          <li><a>{{ item.enrollYear }}</a></li>
+          <li><a><button @click="onClickSignout()">로그아웃</button></a></li>
+        </div>
+        <div v-else>
+          <li><a><button @click="onClickSignin()">로그인</button></a></li>
+        </div>
       </md-table-row>
     </md-table>
   </div>
@@ -22,11 +28,31 @@ export default {
     return {
       profile: [
         {
+          login: null,
           name: localStorage.getItem("userName"),
           enrollYear: localStorage.getItem("userEnrollYear")
         }
       ]
     };
   },
+  methods: {
+    getProfile() {
+      if ( localStorage.getItem("userName") != null ) {
+        this.login = true;
+      }
+      else {
+        this.login = false;
+      }
+    },
+    onClickSignin() {
+      this.$router.push("/signin");
+    },
+    onClickSignout() {
+      this.$router.push("/signout");
+    }
+  },
+  created() {
+    this.getProfile();
+  }
 };
 </script>
