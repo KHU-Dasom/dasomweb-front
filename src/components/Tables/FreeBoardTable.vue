d8e329604c22fcfe566a0b5d31763fa929be68d4
Freckie <freckie@frec.kr>
2020-03-04 16:53:55 +0900

    Fix:

--- src/components/Tables/FreeBoardTable.vue
+++ src/components/Tables/FreeBoardTable.vue
@@ -15,9 +15,9 @@
         <md-table-cell md-label="ID" @click="onclickArticle()">{{
           item.id
         }}</md-table-cell>
-        <md-table-cell md-label="제목">{{ item.title }}</md-table-cell>
+        <md-table-cell md-label="제목">{{ item.short_title }}</md-table-cell>
         <md-table-cell md-label="작성 시간">{{
-          item.published_at
+          item.published_at_kor
         }}</md-table-cell>
         <md-table-cell md-label="작성자">{{ item.author_name }}</md-table-cell>
         <md-table-cell md-label="조회수">{{ item.views }}</md-table-cell>
