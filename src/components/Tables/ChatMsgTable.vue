4cd1cf8e937bba9e9fcbba7efd2e2e94bbd3fb92
inhun <torihome1@naver.com>
2020-03-16 00:32:33 +0900

    Fix chat

--- src/components/Tables/ChatMsgTable.vue
+++ src/components/Tables/ChatMsgTable.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+  <infinite-loading direction="top" @infinite="infiniteHandler" spinner="waveDots"></infinite-loading>
     <md-list v-for="(msg, index) in msgs" v-bind:key="index">
       <md-list-item>
         <md-avatar class="md-avatar-icon" style="font-size: 15px;">{{ msg.from }}</md-avatar>
--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -1,17 +1,29 @@
 <template>
-  <div class="content" :key="$route">
+  <div class="content main-content">
     <div class="md-layout">
       <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100">
-        <md-card class="md-card-plain">
+        <md-card>
           <md-card-header data-background-color="pantone-provence">
             <h4 class="title">채팅방</h4>
           </md-card-header>
-          <md-card-content>
-            <ChatMsgTable :msgs="msgDatas"></ChatMsgTable>
+          <md-card-content class="md-scrollbar">
+          <div>
+
+            <md-list v-for="(msg, index) in msgs" v-bind:key="index">
+            <!--<infinite-loading direction="top" @infinite="infiniteHandler" spinner="waveDots"></infinite-loading>-->
+              <md-list-item>
+                <md-avatar class="md-avatar-icon" style="font-size: 15px;">{{ msg.from.slice(1,3) }}</md-avatar>
+                <div class="md-list-item-text">
+                  <span>{{msg.message}}</span>
+                  <span>{{msg.sendtime}}</span>
+                </div>
+              </md-list-item>
+            </md-list>
+          </div>
           </md-card-content>
         </md-card>
         <md-field>
-          <md-textarea v-model="message" style="resize:unset; min-height:50px; height:50px" @keydown.enter="sendmsg"></md-textarea>
+          <md-textarea v-model="sendmessage" style="resize:unset; min-height:50px; height:50px" @keydown.enter="sendmsg"></md-textarea>
           <md-button class="md-dense md-provence pagination-button" @click="sendmsg">send</md-button>
         </md-field>
       </div>
@@ -20,47 +32,47 @@
 </template>
 
 <script>
-import { mapState } from "vuex";
-import { ChatMsgTable } from "@/components";
 export default {
-  components: {
-    ChatMsgTable
-  },
   data(){
     return {
       conn: null,
-      message: "",
-      status: "disconnected"
+      sendmessage: "",
+      status: "disconnected",
+      msgData: "",
+      idx: 0,
+      msgs: []
     }
-  },
-  computed: {
-    ...mapState({
-      "msgDatas": state => state.msgDatas
-    })
   },
   created() {
     this.wsconnect()
   },
   methods: {
     wsconnect() {
-      this.socket = new WebSocket("ws://222.251.229.213.:8081/ws");
+      this.socket = new WebSocket("ws://222.251.229.213:8082/ws");
       this.socket.onopen = () => {
-        this.status = "connected"
+        this.status = "connected";
+        this.getmsg();
 
         this.socket.onmessage = ({data}) => {
-          var message = JSON.parse(data);
-          this.$store.commit("PushMsgData", {message})
+
+          var templist = JSON.parse(data);
+          console.log(templist);
+          for(var i=0; i<templist.length; i++) {
+            this.msgData = templist[i]
+            this.msgs.push(this.msgData);
+            console.log(this.msgData);
+          }
         }
       }
     },
     sendmsg() {
+      console.log(this.sendmessage)
       if (this.status == "disconnected") {
         return false;
       }
-      if (this.message == null) {
+      if (this.sendmessage == null) {
         return false;
       }
-      //var sendtime = this.$moment(new Date()).format("HH:mm a");
       var d = new Date();
       var hour = d.getHours();
       var minute = d.getMinutes();
@@ -76,13 +88,29 @@ export default {
         time = "오전 " + String(hour) + ":" + minute;
       }
       var realmsg = {
-        id: localStorage.userID,
-        message: this.message,
-        from: localStorage.userName.slice(1,3),
-        sendtime: time
-      };
+        type: "message",
+        data: {
+          message: this.sendmessage,
+          from: this.$store.state.userName,
+          sendtime: time,
+          user_id: this.$store.state.userID
+        }
+      }
       this.socket.send(JSON.stringify(realmsg));
-      this.message="";
+      this.sendmessage="";
+      this.idx = this.idx + 1;
+      return false;
+    },
+    getmsg() {
+      var realmsg = {
+        type: "command",
+        data: {
+          idx: this.idx
+        }
+      }
+      this.idx = this.idx + 10;
+
+      this.socket.send(JSON.stringify(realmsg));
       return false;
     }
   }
@@ -90,22 +118,16 @@ export default {
 
 </script>
 
-<style>
-#log {
-    background: white;
-    margin: 0;
-    padding: 0.5em 0.5em 0.5em 0.5em;
-    top: 0.5em;
-    left: 0.5em;
-    right: 0.5em;
-    bottom: 3em;
-    overflow: auto;
-    height: 50em;
-    width: 90em;
-}
+<style  lang="scss" scoped>
 
 .md-textarea {
   resize: unset;
+}
+
+.md-card-content {
+  max-width: 100%;
+  max-height: 600px;
+  overflow: auto;
 }
 
 </style>
